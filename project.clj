0ebff7ed76736192e5bb772e1fa6eb9a44298efc
Artem Kornienko <send.this.to.moff@gmail.com>
2013-10-12 17:33:06 +0300

    Updated clj-http

--- project.clj
+++ project.clj
@@ -5,4 +5,4 @@
   :license {:name "The MIT License (MIT)"
             :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [clj-http "0.7.2"]])
+                 [clj-http "0.7.7"]])
